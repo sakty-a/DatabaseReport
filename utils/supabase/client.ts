408e638ed2efbc303971a3709b6ef92bbba23d6a
Saktya <alfisyasaktya.1704326@students.um.ac.id>
2026-06-10 18:39:44 +0700

    chore: remove hardcoded Supabase credentials and update README
    Replaced hardcoded credentials with environment variables for security and updated the README to provide setup instructions for local development.

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;=https://xputthytezdkrxcrqyzw.supabase.co
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;=sb_publishable_GBheCKI13fi6VrSR58MgFw_m81pvoKK
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 export const createClient = () =>
   createBrowserClient(
